5fd493e79f38b6e273630fe3313fbdfe40417d48
Tw1stTheBones <molokomax93@gmail.com>
2021-04-27 17:44:11 +0300

    Add driver

--- src/test/java/TestTelemar/Helpers/DriverHolder.java
+++ src/test/java/TestTelemar/Helpers/DriverHolder.java
@@ -1,6 +1,6 @@
 package TestTelemar.Helpers;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
